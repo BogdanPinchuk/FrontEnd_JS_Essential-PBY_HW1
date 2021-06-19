0c4d0d18e018c206d80100db6668efa174e505cd
Bogdan <pinchuk.brus@windowslive.com>
2021-06-19 09:48:01 +0300

    Addded taksk 3

---
+++ LesApp2/Resources/task.js
@@ -0,0 +1,21 @@
+// message
+console.log("Завдання.");
+console.log("Знайти суму: a, b і c.");
+console.log("Дано:");
+
+// variables // створення змінних працюює подібно як  в C#
+let a = 1, 
+b = 2, 
+c = 3;
+
+// message
+console.log("a = " + a);
+console.log("b = " + b);
+console.log("c = " + c);
+console.log("Розв'язок:");
+
+let s = a + b + c;
+
+// message
+console.log("Sum = a + b + c = " + s);
+console.log("Відповідь: sum = " + s + ".");
